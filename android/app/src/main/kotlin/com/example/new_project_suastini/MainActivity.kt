09133912b27f4d118625ce3ae5dd5cb144589739
Suastiniputu <psuastini28@gmail.com>
2024-10-06 16:48:28 +0800

    Flutter project

---
+++ android/app/src/main/kotlin/com/example/new_project_suastini/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_project_suastini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ suastini_project/android/app/src/main/kotlin/com/example/suastini_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.suastini_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
